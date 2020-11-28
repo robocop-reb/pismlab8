ef5feede979153d6d858ca3a3bc1e6456304cc2a
Valera <valeralishko@gmail.com>
2020-11-28 12:39:33 +0300

    removed Director.java

--- src/Application.java
+++ src/Application.java
@@ -10,9 +10,5 @@ public class Application {
         Unit soldier = builder.getResult();
         director.buildTank(builder);
         Unit tank = builder.getResult();
-        System.out.println("Unit built:\n" + plane.getUnitType());
-        System.out.println("Plane health:\n" + plane.getUnitHealth());
-        System.out.println("Unit built:\n" + soldier.getUnitType());
-        System.out.println("Unit built:\n" + tank.getUnitType());
     }
 }
